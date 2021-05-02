a2fd23286bdb7331bac137a7d74958e0969766f3
sanja-k-hristov <sanja.k.hristov@gmail.com>
2021-05-02 22:20:34 +0200

    homework added

---
+++ homework01/RPS/RPS/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace RPS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string answer = "";
+            int userWin = 0;
+            int computerWin = 0;
+
+            Console.WriteLine("Welcome to RPS game");
+
+            while (answer != "NO")
+            {
+                Console.WriteLine("ROCK , PAPER or SCISSOR");
+                string[] choices = new string[3] { "ROCK", "PAPER", "SCISSOR" };
+                Random rnd = new Random();
+                int n = rnd.Next(0, 3);
+                Console.WriteLine("Enter your choice:");
+                string userChoice = Console.ReadLine().ToUpper();
+                Console.WriteLine("Computer:" + choices[n]);
+
+                if (userChoice == "ROCK" && choices[n] == "SCISSOR")
+                {
+                    Console.WriteLine("User wins");
+                    userWin += 1;
+                }
+                else if (userChoice == "ROCK" && choices[n] == "PAPER")
+                {
+                    Console.WriteLine("Computer wins");
+                    computerWin += 1;
+                }
+                else if (userChoice == "PAPER" && choices[n] == "ROCK")
+                {
+                    Console.WriteLine("User wins");
+                    userWin += 1;
+                }
+                else if (userChoice == "PAPER" && choices[n] == "SCISSOR")
+                {
+                    Console.WriteLine("Computer Wins");
+                    computerWin += 1;
+                }
+                else if (userChoice == "SCISSOR" && choices[n] == "ROCK")
+                {
+                    Console.WriteLine("Computer Wins");
+                    computerWin += 1;
+                }
+                else if (userChoice == "SCISSOR" && choices[n] == "PAPER")
+                {
+                    Console.WriteLine("User wins");
+                    userWin += 1;
+                }
+                else
+                {
+                    Console.WriteLine("Same choices");
+                }
+                Console.WriteLine("Do u want to continue(YES/NO):");
+                answer = Console.ReadLine().ToUpper();
+                Console.WriteLine("---------------------------------------");
+            }
+            Console.WriteLine("User wins " + userWin + " times");
+            Console.WriteLine("Computer wins " + computerWin + " times");
+        }
+    }
+}
---
+++ homework01/WorkDay/WorkDay/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace WorkDay
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                string answer = "";
+
+                while (answer != "NO")
+                {
+                  
+                    Console.WriteLine("Enter your date to check if it is working day or not, in the following format DD-MM-YYYY");
+
+                    string userInput = Console.ReadLine();
+                    bool tryParseDate = DateTime.TryParse(userInput, out DateTime parsedDate);
+
+                    if (tryParseDate == true)
+                    {
+                        if ((parsedDate.Day == 1 && parsedDate.Month == 1) || (parsedDate.Day == 7 && parsedDate.Month == 1) || (parsedDate.Day == 20 && parsedDate.Month == 4) || (parsedDate.Day == 1 && parsedDate.Month == 5) || (parsedDate.Day == 25 && parsedDate.Month == 5) || (parsedDate.Day == 3 && parsedDate.Month == 8) || (parsedDate.Day == 8 && parsedDate.Month == 9) || (parsedDate.Day == 12 && parsedDate.Month == 10) || (parsedDate.Day == 23 && parsedDate.Month == 10) || (parsedDate.Day == 8 && parsedDate.Month == 12) || parsedDate.DayOfWeek == DayOfWeek.Saturday || parsedDate.DayOfWeek == DayOfWeek.Sunday)
+                        {
+                            Console.WriteLine($"You entered: {parsedDate} - non-working day");
+                        }
+                        else
+                        {
+                            Console.WriteLine($"You entered: {parsedDate} - working day");
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid input, try again...");
+                    }
+                    Console.WriteLine("--------------------------------------------------");
+                    Console.WriteLine("Do u want to continue(YES/NO):");
+                    answer = Console.ReadLine().ToUpper();
+                    Console.Clear();
+                }   
+        }
+    }
+}
